472e808d2217e0d10ccb7ad3e09edae8d9c7e6e6
jarwan6 <jarwanabood6@gmail.com>
2026-06-06 00:23:42 +0300

    Create cmake_clean.cmake

---
+++ cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeArena.dir/src/main.cpp.obj"
+  "CMakeFiles/SnakeArena.dir/src/main.cpp.obj.d"
+  "SnakeArena.exe"
+  "SnakeArena.exe.manifest"
+  "SnakeArena.pdb"
+  "libSnakeArena.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeArena.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
